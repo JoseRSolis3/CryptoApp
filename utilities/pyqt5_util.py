cf3a9c87b3f9bb666312c7142c288603895be424
Jose Solis <jrs31997@outlook.com>
2025-04-27 06:19:57 -0500

    Update pyqt5_util.py

--- utilities/pyqt5_util.py
+++ utilities/pyqt5_util.py
@@ -1,21 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
 
 title = "Crypto App"
 geometry = (100, 100, 500, 300)
+label_text = "This is a label"
 
 class Window:
-    def __init__(self, title, geometry):
+    def __init__(self, title, geometry, label_text = None, button_text = None):
+        self.window_setup(title, geometry)
+    
+        self.win_title()
+        self.win_geometry()
+
+        self.win_components(label_text, button_text)
+
+        self.window.show()
+        sys.exit(self.app.exec_())
+
+    def window_setup(self, title, geometry):
         self.window_title = title
         self.geometry = geometry
         self.app = self.create_app_obj()
         self.window = self.create_window()
-
-        self.win_title()
-        self.win_geometry()
-
-        self.window.show()
-        sys.exit(self.app.exec_())
+        self.layout = QVBoxLayout()        
 
     def create_app_obj(self):
         return QApplication(sys.argv)
@@ -27,7 +34,24 @@ class Window:
         self.window.setWindowTitle(self.window_title)
 
     def win_geometry(self):
-        self.window.setGeometry(*geometry)
-    
-Window(title, geometry)
+        self.window.setGeometry(*self.geometry)
+
+    def win_components(self, label_text = None, button_text = None):
+        if label_text:
+            for text in label_text:
+                self.win_label(text)
+        
+        if button_text:
+            for text in button_text:
+                self.win_button(text)
+
+    def win_label(self, label_text):
+        label = QLabel(label_text)
+        self.layout.addWidget(label)
+
+    def win_button(self):
+        button = QPushButton("Click Me!")
+        self.layout.addWidget(button)
+
+Window(title, geometry, label_text)
 
